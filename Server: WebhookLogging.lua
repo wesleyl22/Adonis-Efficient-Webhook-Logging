4b61f92659b09eada398e8f390f28a1111fc802e
Wesley <69184235+wesleyl22@users.noreply.github.com>
2024-07-01 17:06:49 +0700

    Create Server: WebhookLogging.lua

---
+++ Server: WebhookLogging.lua
@@ -0,0 +1,68 @@
+local set = require(script.Parent.Parent.Settings)
+local Loc = game:GetService("ServerStorage")["REMXZ-AdminLogging-STORAGE"]
+
+server = nil 
+service = nil
+
+local PlaceName = game:GetService('MarketplaceService'):GetProductInfo(game.PlaceId).Name
+local HTTPS = game:GetService("HttpService")
+
+
+return function()
+    service.Events.CommandRan:Connect(function(player, Data)
+
+        local lvl = nil
+        local command = nil
+        local success = nil
+
+        for i,v in pairs(Data) do
+            if i == "Success" then
+                success = v
+            end
+            if i == "Message" then
+                command = v
+            end
+            if type(v) == "table" then
+                for e,n in pairs(v) do
+                    if e == "Level" then
+                        lvl = "Player"
+                        
+                        for a,b in pairs(set.Settings.Ranks) do
+                            if n >= b.Level then
+                                lvl = a
+                            end
+                        end
+                    end
+                end
+            end
+        end
+        
+        if success then
+            if Loc.Count.Value >= 10 then
+                wait(1)
+            end
+
+            Loc.Count.Value += 1
+            
+            local Folder = Instance.new("Folder", Loc.Data)
+            Folder.Name = Loc.Count.Value
+
+            local User = Instance.new("StringValue", Folder)
+            User.Name = "User"
+            User.Value = player.UserId
+
+            local Command = Instance.new("StringValue", Folder)
+            Command.Name = "Command"
+            Command.Value = command
+
+            local Level = Instance.new("StringValue", Folder)
+            Level.Name = "Level"
+            Level.Value = lvl
+
+            local Time = Instance.new("StringValue", Folder)
+            Time.Name = "Time"
+            Time.Value = service.GetTime()
+        end
+        
+    end)
+end
